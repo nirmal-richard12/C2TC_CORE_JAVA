5ca5dbdd9d58c4fe59bad6f32b4228cb249a93ab
Richard <nirmalrichard1810@gmail.com>
2024-10-25 19:21:06 +0530

    dayone and daytwo

---
+++ C2TC_RICHARD/src/com/tns/ifet/daytwo/WhileDemo.java
@@ -0,0 +1,16 @@
+package com.tns.ifet.daytwo;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int i=99; 
+		  while(i<=100) 
+		  { 
+		   System.out.println(i); 
+		   i++;
+		  }
+		// TODO Auto-generated method stub
+
+	}
+
+}
