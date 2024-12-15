1d35f03e6f06736904506721fc81b855ece59d3d
RICHARD HP <RICHARD HP@RICHARD>
2024-12-15 12:20:16 +0530

    assignment1

---
+++ C2TC_RICHARD/src/com/tns/ifet/assignment1/employeeutilities/EmployeeUtilities.java
@@ -0,0 +1,19 @@
+package com.tns.ifet.assignment1.employeeutilities;
+import com.tns.ifet.assignment1.employee.Developer;
+import com.tns.ifet.assignment1.employee.Employee;
+import com.tns.ifet.assignment1.employee.Manager;
+public class EmployeeUtilities {
+ public static void printEmployeeDetails(Employee employee) {
+ System.out.println("Employee Name: " + employee.getName());
+ System.out.println("Employee ID: " + employee.getEmployeeId());
+ System.out.println("Salary: " + employee.getSalary());
+ if (employee instanceof Manager) {
+ Manager manager = (Manager) employee;
+ System.out.println("Department: " + manager.getDepartment());
+ } else if (employee instanceof Developer) {
+ Developer developer = (Developer) employee;
+ System.out.println("Programming Language: " + developer.getProgrammingLanguage());
+ }
+ System.out.println();
+ }
+}
