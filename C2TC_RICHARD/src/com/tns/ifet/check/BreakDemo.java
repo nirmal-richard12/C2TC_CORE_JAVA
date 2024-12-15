4851f5d504668f7d2286f77f991f68f0f45c81f6
RICHARD HP <RICHARD HP@RICHARD>
2024-12-15 12:43:17 +0530

    check

---
+++ C2TC_RICHARD/src/com/tns/ifet/check/BreakDemo.java
@@ -0,0 +1,15 @@
+package com.tns.ifet.check;
+
+
+
+public class BreakDemo { 
+ public static void main(String[] args) { 
+ 
+  for (int i = 2; i < 10; i++) { 
+   if (i == 5) 
+    break; 
+   System.out.println(i); 
+  } 
+ 
+ } 
+}
